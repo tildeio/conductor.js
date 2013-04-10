3fe30d32999b417346ba250f9d08516e6a4bef13
David J. Hamilton <davidjh@hjdivad.com>
2013-04-10 12:41:52 -0700

    Remove dead code.

--- example/cards/video/card.js
+++ example/cards/video/card.js
@@ -1,7 +1,4 @@
-/*global Handlebars*/
-
 Conductor.require('/example/libs/jquery-1.9.1.js');
-Conductor.require('/example/libs/handlebars-1.0.0-rc.3.js');
 // Youtube doesn't allow iframe_api via CORS
 //Conductor.require('https://www.youtube.com/iframe_api');
 Conductor.requireCSS('/example/cards/video/style.css');
