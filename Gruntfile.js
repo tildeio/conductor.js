904dd2b18f37569f7e9369a5bd8ff74a645cd196
David J. Hamilton <davidjh@hjdivad.com>
2013-05-01 16:00:36 -0700

    Grunt task for tutorial retagging.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function(grunt) {
+  var exec = require('child_process').exec;
+
   // Alias tasks for the most common sets of tasks.
   // Most of the time, you will use these.
 
@@ -14,6 +16,32 @@ module.exports = function(grunt) {
 
   // Run a server. This is ideal for running the QUnit tests in the browser.
   this.registerTask('server', ['concat:tests', 'build-dev', 'connect', 'watch']);
+
+  this.registerTask('tutorial:retag', function () {
+    var done = this.async();
+
+    exec('git log -n 1 --format=%s | grep -q -p ^Tutorial', function (sin, sout, serr) {
+      if (sin instanceof Error) {
+        grunt.fail.fatal("The commit message of HEAD does not begin with 'Tutorial:'.  You must run 'tutorial:retag' from the last tutorial commit.");
+      }
+    });
+
+    exec('git rev-list --reverse tutorial-setup..', function (sin, sout, serr) {
+      var lastSha, cmd;
+      sout.replace(/\n$/, '').split("\n").forEach(function (sha, i) {
+        var cmd = "git tag -f tutorial-" + (i+1) + " " + sha;
+        exec(cmd);
+        grunt.log.write(cmd + "\n");
+        lastSha = sha;
+      });
+
+      cmd = "git tag -f tutorial " + lastSha;
+      exec(cmd);
+      grunt.log.write(cmd + "\n");
+
+      done();
+    });
+  });
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
