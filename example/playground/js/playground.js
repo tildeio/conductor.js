3416e296a5dea775746e3f81e1594b11cad58b20
David J. Hamilton <davidjh@hjdivad.com>
2013-05-01 14:48:37 -0700

    Tutorial: Update child card data.

--- example/playground/js/playground.js
+++ example/playground/js/playground.js
@@ -52,7 +52,7 @@ window.Playground = {
         }
       }
     });
-    this.conductor.loadData('../cards/tutorial/ad_card.js', '1', { videoId: '4d8ZDSyFS2g'});
+
     this.addCard('../cards/tutorial/ad_card.js', 1, ['video', 'survey']);
   }
 };
