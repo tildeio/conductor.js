ad5e0984ff3b2fda25e2a0acbc1925deb4d58858
Cyril Fluck <github@fluck.fr>
2013-07-10 09:47:59 -0700

    Fix asynchronous tests

--- test/tests/metadata_test.js
+++ test/tests/metadata_test.js
@@ -18,20 +18,26 @@ module("Metadata Service", {
 });
 
 test("A card can return metadata for some type", function() {
+  stop();
+
   card.promise.then(async(function() {
     return card.metadataFor('document');
   })).then(async(function(documentMetadata) {
     equal(documentMetadata.title, "Rails is omakase");
+    start();
   }));
 });
 
 test("A card carn return all of its metadata at once", function() {
+  stop();
+
   card.promise.then(async(function() {
     return card.metadataFor('*');
   })).then(async(function(metadata) {
     equal(metadata['document:title'], "Rails is omakase");
     equal(metadata['image:width'], 100);
     equal(metadata['image:height'], 100);
+    start();
   }));
 });
 
