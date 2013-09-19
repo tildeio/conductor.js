a5d9a1a0a7fca5afcc68ba60f3aeeb4ca3482fa0
Cyril Fluck <github@fluck.fr>
2013-09-19 10:48:48 -0700

    Fix watch task for Grunt

--- configurations/watch.js
+++ configurations/watch.js
@@ -1,4 +1,4 @@
 module.exports = {
   files: ['lib/**/*.js', 'vendor/*', 'test/tests/*', 'test/helpers/*', 'test/fixtures/*', 'node_modules/jsframe/*'],
-  tasks: ['build', 'concat:tests']
+  tasks: ['prepare_test']
 };
